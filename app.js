65a4ce34f1502f4f6e05448cb08addf60b03bb59
Henok Debesay <henokdebesay@gmail.com>
2024-02-01 20:45:49 -0800

    working on the js file

---
+++ app.js
@@ -0,0 +1,50 @@
+class Book {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title: "Book One",
+                author: "John Doe",
+                isbn: "343434"
+            },
+            {
+                title: "Book Two",
+                author: "John Doe",
+                isbn: "4545454"
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.foreach((book) => UI.addBookToList(book));
+
+    }
+
+    static addBookToList(book){
+        const list = document.querySelector("#book-list");
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+
+console.log("working");
