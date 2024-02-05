a26b12f02f5f9494c12388b35bb0d98be328a082
Henok Debesay <henokdebesay@gmail.com>
2024-02-04 19:14:15 -0800

    testing functions

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let submit = document.getElementById("submit");
 let inputs = document.getElementById("inputs")
-let bookTitle = inputs.querySelectorAll('input')[0];
-let author = inputs.querySelectorAll('input')[1];
-console.log(author);
+let bookTitle = inputs.getElementsByClassName('inputs')[0];
+let author = inputs.getElementsByClassName('inputs')[1];
+let table = document.getElementsByTagName("table")[0];
+let tBody = document.getElementById("book-list");
+
+
+console.log(inputs);
+
+
+submit.addEventListener("click", () => {
+    if( bookTitle.value == "" && author.value == ""){
+        console.log("try again")
+    }else {
+        console.log("good job");
+    }
+})
+
+
 
