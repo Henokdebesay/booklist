42df0584e87e4af6bb99aa9bff839cd299cf2fed
Henok Debesay <henokdebesay@gmail.com>
2024-02-04 18:32:47 -0800

    working on js variable naming

---
+++ app.js
@@ -0,0 +1,6 @@
+let submit = document.getElementById("submit");
+let inputs = document.getElementById("inputs")
+let bookTitle = inputs.querySelectorAll('input')[0];
+let author = inputs.querySelectorAll('input')[1];
+console.log(author);
+
