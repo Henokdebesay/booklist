eebcbf97c996944bc68c8c7fe6518ab46a0f4754
Henok Debesay <henokdebesay@gmail.com>
2024-02-04 19:47:58 -0800

    list working

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 let submit = document.getElementById("submit");
 let inputs = document.getElementById("inputs")
-let bookTitle = inputs.getElementsByClassName('inputs')[0];
-let author = inputs.getElementsByClassName('inputs')[1];
+let bookTitle = inputs.getElementsByClassName('input')[0];
+let author = inputs.getElementsByClassName('input')[1];
 let table = document.getElementsByTagName("table")[0];
 let tBody = document.getElementById("book-list");
 
-
-console.log(inputs);
-
+// let row = document.createElement("tr")
+// let td = document.createElement("td")
+// console.log(td);
 
 submit.addEventListener("click", () => {
-    if( bookTitle.value == "" && author.value == ""){
+    if( !bookTitle.value && !author.value){
         console.log("try again")
     }else {
-        console.log("good job");
+        let row = document.createElement("tr")
+
+        let td1 = document.createElement("td")
+        td1.textContent = bookTitle.value;
+        let td2 = document.createElement("td")
+        td2.textContent = author.value;
+
+        row.appendChild(td1)
+        row.appendChild(td2)
+
+        console.log(row);
+
+        table.appendChild(row);
+       
     }
 })
 
